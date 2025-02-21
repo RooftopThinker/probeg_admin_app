372b607cc9bc7aafdfc6460754b87661b2a4dc91
RooftopThinker <keysec@keemail.me>
2025-02-21 23:36:27 +0300

    website design done

--- monitor/views.py
+++ monitor/views.py
@@ -7,12 +7,48 @@ import json
 import config
 import asyncio
 from monitor.utils import get_client_ip, validate_token
+from django.contrib.humanize.templatetags.humanize import intcomma
+import random
 
 
 def dashboard_callback(request, context):
-    context.update({
-        "custom_variable": "value",
-    })
+    alive_users = User.objects.filter(is_bot_blocked=False).count()
+    all_users = User.objects.count()
+    users_registered = User.objects.exclude(full_name__isnull=True).exclude(full_name__exact='').count()
+    users_in_paid_membership = (User.objects.exclude(subscription_till__isnull=True).
+                                exclude(subscription_till__gte=datetime.datetime.today())).count()
+    users_applied = User.objects.filter(applied_to_paid_membership=True).count()
+    users_approved = User.objects.filter(is_accepted_to_paid_membership=True).count()
+
+    data = {"bars": [
+        {
+            "title": "Alive users",
+            "description": f"{alive_users} out of {all_users}",
+            "value": alive_users // all_users * 100,
+        },
+        {
+            "title": "Registered users",
+            "description": f"{users_registered} out of {all_users}",
+            "value": alive_users // users_registered * 100,
+        }
+        ,
+        {
+            "title": "Users applied for paid membership",
+            "description": f"{users_applied} out of {all_users}",
+            "value": users_applied // all_users * 100,
+        }
+        , {
+            "title": "Users approved for paid membership",
+            "description": f"{users_approved} out of {all_users}",
+            "value": users_approved // all_users * 100,
+        },
+        {
+            "title": "Members of paid membership",
+            "description": f"{users_in_paid_membership} out of {all_users}",
+            "value": users_in_paid_membership // all_users * 100,
+        }
+        ]}
+    context.update(data)
 
     return context
 
@@ -24,7 +60,6 @@ def test(request):
         # asyncio.run(bot.send_message(text=str(list(Receipt.objects.get())) + 'SOSI HUETS', chat_id=1186221701))
         return HttpResponse(status=200)
     if not validate_token(request_json, config.PASSWORD):
-
         return HttpResponse(status=400)
     try:
         order = Receipt.objects.get(order_id=request_json['OrderId'])
@@ -38,6 +73,7 @@ def test(request):
     order.telegram_id.subscription_till = datetime.datetime.now() + datetime.timedelta(days=365)
     order.telegram_id.save()
     bot = Bot(token=config.BOT_TOKEN)
-    asyncio.run(bot.send_message(text="Вам дан доступ к платным форматам! [ссылка]", chat_id=order.telegram_id.telegram_id))
+    asyncio.run(
+        bot.send_message(text="Вам дан доступ к платным форматам! [ссылка]", chat_id=order.telegram_id.telegram_id))
     asyncio.run(bot.session.close())
     return HttpResponse(status=200)
--- probeg_admin_website/settings.py
+++ probeg_admin_website/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -71,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'probeg_admin_website.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
@@ -85,7 +83,6 @@ DATABASES = {
         'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -105,7 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
@@ -117,7 +113,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
@@ -128,9 +123,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
 UNFOLD = {
     "SITE_TITLE": "PROБЕГ",
     "SITE_HEADER": "PROБЕГ",
+    "DASHBOARD_CALLBACK": "monitor.views.dashboard_callback"
 }
-
